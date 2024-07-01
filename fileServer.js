046c3188f0ac2f5d4c875a718d43fd9c53d784c7
Manicharan Kollipara <manicharan@Manicharans-Laptop.local>
2024-07-01 23:32:47 +0530

    Added a file called todoServer.solution.js

--- fileServer.js
+++ fileServer.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const filesDir = path.join(__dirname, "files");
 
